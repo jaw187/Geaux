26cb798f1ad1e5c75f3e607695133bb657e8c95f
James Weston <james@tenseven.org>
2012-01-24 09:41:41 -0500

    Rewritten with Events

--- geaux.js
+++ geaux.js
@@ -1,47 +1,35 @@
-var PingManager = require('./node-ping').PingManager;
-pm=new PingManager();
-
-var mongo=require('./mongodb-wrapper');
-var db = mongo.db('10.7.0.7',27017,'geaux');
-db.collection("pings");
+var EventEmitter = require('events').EventEmitter,
+    util = require('util');
 
 Host = function (host) {
-  this.host=host;
-  var that=this;
+  var PingManager = require('./node-ping').PingManager;
+  var pm = new PingManager();
+
+  this.host = host;
+  var self = this;
   pm.start(function () {
     var pinger = this.createOnePinger(1000,[host]);
     pinger.on('ping', function (mo) {
       if (mo.rcv > 0) {
-        that.up(mo.avg);
+        if (typeof mo.avg === "undefined") {
+          if (typeof mo.max === "undefined")
+            rtt = mo.min;
+          else rtt = mo.max;
+        }
+        else rtt = mo.avg
+
+        if (rtt) self.emit('response',rtt);
+        else console.log("asdf");
       }
-      else that.down();
+      else self.emit('drop');
     });
     pinger.start();
   });
 }
 
-Host.prototype.up = function (rtt) {
-  //console.log(this.host + ' - ' + rtt);
-  db.pings.save({
-    who: this.host,
-    when: new Date(),
-    rtt: rtt,
-    up: true
-  });
+util.inherits(Host,EventEmitter);
+
+exports.Host = function (host) {
+  return new Host(host);
 }
 
-Host.prototype.down = function () {
-  //console.log(this.host + ' is down');
-  db.pings.save({
-    who: this.host,
-    when: new Date(),
-    rtt: -1,
-    up: false
-  });
-}
-
-test=new Host('216.182.1.1');
-two=new Host('core1.tdc.nwt.tellurian.net');
-three=new Host('core1.tdc.los.tellurian.net');
-four=new Host('8.8.8.8');
-five=new Host('4.2.2.2');
